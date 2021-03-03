d0cae0fc127151d5845e7543b1f7d59714f0e867
Hyeshik Chang <hyeshik@snu.ac.kr>
2021-03-03 23:52:25 +0900

    Add setup script.

---
+++ tools/setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+CONDA_PREFIX=/root/conda
+
+# Copy overlay files
+tar -C overlay -cf - . | tar -C / -xvf -
+
+# Download and install miniconda
+CONDAINSTALLER=miniconda3.sh
+
+if [ ! -f $CONDAINSTALLER ]; then
+    wget -O $CONDAINSTALLER \
+        https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+fi
+bash $CONDAINSTALLER -b -f -p $CONDA_PREFIX
+
+
+# Set-up a new conda environment
+$CONDA_PREFIX/bin/conda create -n lab -y \
+  pip scipy numpy biopython matplotlib pandas pysam cython \
+  samtools bwa bedtools star
+
+
+# Activate our .bashrc
+if ! grep DEFAULT_CONDA $HOME/.bashrc >/dev/null 2>&1; then
+  cat $HOME/.bashrc.biolab >> $HOME/.bashrc
+fi
+
+# vim: sts=4 et sw=4
