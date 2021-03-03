77e3b828f1dec79e130db91ba1f727313b4d281b
Hyeshik Chang <hyeshik@snu.ac.kr>
2021-03-04 00:22:58 +0900

    Point the right path to conda bin.

--- tools/activate_conda.py
+++ tools/activate_conda.py
@@ -2,17 +2,22 @@
 import subprocess as sp
 import os, re
 
+CONDA_PREFIX = "/root/conda"
 CONDA_ENV = "lab"
 
 envvars = sp.check_output('''
-    eval "$(conda shell.bash hook)";
+    eval "$({}/bin/conda shell.bash hook)";
     conda activate {};
-    export'''.format(CONDA_ENV),
+    export'''.format(CONDA_PREFIX, CONDA_ENV),
     shell=True, executable='/bin/bash')
 
 for line in envvars.decode().splitlines():
   namevalue = line.split(None, 2)[-1]
   name, value = namevalue.split('=', 1)
   os.environ[name] = value[1:-1]
+
+# Prioritize some commands from Colab
+os.environ['PATH'] = (os.environ['HOME'] + '/.bin.priority:' +
+                      os.environ['PATH'])
 
 print("Activated conda environment `{}'!".format(CONDA_ENV))
